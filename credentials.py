54a1624fc61b34ef0401f72d7d18d181761d766d
mohdabrar07 <mohdabrarahammad@gmail.com>
2024-11-30 12:03:05 +0530

    Initial commit for my project

---
+++ credentials.py
@@ -0,0 +1,7 @@
+client_ID='9945d40556724c04842a082f46641a3a'
+      # Fill in with the value from the Spotify Developer Dashboard
+client_SECRET='9c362372dc134d2db588d65346665171'   # Fill in with the value from the Spotify Developer Dashboard
+redirect_URI="http://localhost:5050"  # Fill in with the port you want to use -- MUST be one of the 
+                                                                   # redirect urls as in Spotify Developer Dashboard 
+username='Mohdabrarahammad'        # Fill in with Spotify username you want to use                                                                                                      
+scope = "user-read-private playlist-read-private playlist-modify-public user-top-read"  # Set the scope(s)
